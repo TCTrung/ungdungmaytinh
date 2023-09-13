bf16f26619a8e35a09fab53a6af69feaa4af1de0
TCTrung <trungtrinh1325@gmail.com>
2023-09-13 15:45:48 +0700

    hi

---
+++ my.js
@@ -0,0 +1,20 @@
+    function Add() {
+    let a = +document.getElementById("a").value;
+    let b = +document.getElementById("b").value;
+    document.getElementById("Result").innerHTML = a+b;
+}
+    function Sub() {
+    let a = +document.getElementById("a").value;
+    let b = +document.getElementById("b").value;
+    document.getElementById("Result").innerHTML = a - b;
+}
+    function Mul() {
+    let a = +document.getElementById("a").value;
+    let b = +document.getElementById("b").value;
+    document.getElementById("Result").innerHTML = a * b;
+}
+    function Div() {
+    let a = +document.getElementById("a").value;
+    let b = +document.getElementById("b").value;
+    document.getElementById("Result").innerHTML = a / b;
+}
